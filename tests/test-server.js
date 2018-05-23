9bca0b3b90f52b133a848bd7cc14ab7e69de6643
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-05-23 15:49:26 -0500

    GET integration test

--- tests/test-server.js
+++ tests/test-server.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const {app, runServer, closeServer} = require('../server');
-const should = chai.should();
+const expect = chai.expect;
 
 chai.use(chaiHttp);
 
@@ -12,5 +12,22 @@ describe('Blog Posts', function() {
   });
   after(function() {
     return closeServer();
+  });
+
+  // GET INTEGRATION TEST
+  it('should list items on GET', function() {
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.above(0);
+        res.body.forEach(function(item) {
+          expect(item).to.be.a('object');
+          expect(item).to.have.all.keys(
+            'id', 'title', 'content', 'author', 'publishDate')
+        });
+      });
   });
 });
